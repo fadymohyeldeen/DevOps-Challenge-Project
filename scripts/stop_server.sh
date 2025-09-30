df55fb39e7d40ca7880bbabce2afa6bf8b510036
Fady Mohy El-Deen <fadymohy@zohomail.com>
2025-09-30 14:16:26 +0000

    na

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 echo "ApplicationStop: nothing to stop, exiting successfully."
 exit 0
+#!/bin/bash
+isExistApp=""
+if [[ -n  ]]; then
+sudo systemctl stop httpd.service
+fi
+isExistApp=""
+if [[ -n  ]]; then
+sudo systemctl stop tomcat.service
+fi
+
